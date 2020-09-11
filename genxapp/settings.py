03f1a1520a8aab0a98e82f106e0ba20bb1bfcf45
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2020-09-11 19:58:27 -0300

    setting

--- genxapp/settings.py
+++ genxapp/settings.py
@@ -19,6 +19,8 @@ from decouple import config
 
 from dj_database_url import parse as dburl
 
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -147,3 +149,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 4
 }
+
+django_heroku.settings(locals())
