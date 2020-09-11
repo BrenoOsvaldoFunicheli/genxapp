f1aabac8b78cbd1d86b014f118d20eff1e8ff4ea
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2020-09-11 16:53:28 -0300

    Configuring the app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn genxapp.wsgi --log-file -
--- genxapp/wsgi.py
+++ genxapp/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genxapp.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+# application = get_wsgi_application()
