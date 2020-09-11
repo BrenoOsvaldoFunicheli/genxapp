520094244c6d398ccdf916bacf55003ee88348bf
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2020-09-11 16:53:28 -0300

    Configuring the app

--- genxapp/settings.py
+++ genxapp/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+# heroku deploy
+from decouple import config
+
+from dj_database_url import parse as dburl
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,13 +27,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4paio9el^5%&(_hb)*v+0ntggdqu&@dpwq&qve2xf_m4b-grc$'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['genxapp.herokuapp.com/','0.0.0.0', '127.0.0.1']
 
 # Application definition
 
@@ -78,11 +84,17 @@ WSGI_APPLICATION = 'genxapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql', 'NAME': 'genxapp', 'USER': 'postgres', 'PASSWORD': 'root', 'HOST': 'db', 'PORT': 5432
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql', 'NAME': 'genxapp', 'USER': 'postgres', 'PASSWORD': 'root', 'HOST': 'db', 'PORT': 5432
+#     },
+# }
+
+# heroku setup
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
 
 
 # Password validation
@@ -122,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 REST_FRAMEWORK = {
